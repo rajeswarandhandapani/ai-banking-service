39d403eb6713a0325849e59919474542023a29b2
Rajeswaran Dhandapani <prorajeswaran@gmail.com>
2025-08-08 16:45:00 -0400

    enhance ChatClient configuration to include memory advisor and default system message

--- src/main/java/com/rajeswarandhandapani/aibankingservice/config/ChatClientConfig.java
+++ src/main/java/com/rajeswarandhandapani/aibankingservice/config/ChatClientConfig.java
@@ -1,15 +1,29 @@
 package com.rajeswarandhandapani.aibankingservice.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChatClientConfig {
 
-	@Bean
-	public ChatClient chatClient(ChatClient.Builder builder) {
-		return builder.build();
-	}
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
+        return builder
+                .defaultSystem(
+                        """
+                                You are a helpful AI assistant for a banking service for the BAAS Bank.
+                                Your role is to assist users with their banking queries and transactions.
+                                """
+                )
+                .defaultAdvisors(
+                        SimpleLoggerAdvisor.builder().build(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+                .build();
+    }
 
 }
