90c03e80f58a9f5cea75578daee74dbe687b54c4
Rajeswaran Dhandapani <prorajeswaran@gmail.com>
2025-08-08 13:50:10 -0400

    init project

---
+++ src/main/java/com/rajeswarandhandapani/ai_banking_service/AiBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rajeswarandhandapani.ai_banking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBankingServiceApplication.class, args);
+	}
+
+}
