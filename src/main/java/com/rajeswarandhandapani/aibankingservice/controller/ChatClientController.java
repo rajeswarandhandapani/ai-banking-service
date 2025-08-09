c3d62466f5aa4695a23579801eeea36f66d6f3f9
Rajeswaran Dhandapani <prorajeswaran@gmail.com>
2025-08-09 15:47:09 -0400

    refactor ChatClientController to integrate BankingClient and enhance chat functionality; add endpoint for fetching user accounts

--- src/main/java/com/rajeswarandhandapani/aibankingservice/controller/ChatClientController.java
+++ src/main/java/com/rajeswarandhandapani/aibankingservice/controller/ChatClientController.java
@@ -1,15 +1,23 @@
 package com.rajeswarandhandapani.aibankingservice.controller;
 
-import com.rajeswarandhandapani.aibankingservice.llmtools.DateTimeTools;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.stringtemplate.v4.compiler.CodeGenerator.primary_return;
+
+import com.rajeswarandhandapani.aibankingservice.ai.llmtools.BankingServiceTools;
+import com.rajeswarandhandapani.aibankingservice.ai.llmtools.DateTimeTools;
+import com.rajeswarandhandapani.aibankingservice.services.BankingClient;
+import com.rajeswarandhandapani.aibankingservice.utils.ApplicationSecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @Validated
@@ -18,19 +26,35 @@ public class ChatClientController {
     @Autowired
     private ChatClient chatClient;
 
+    @Autowired
+    private ApplicationSecurityContext applicationSecurityContext;
+
+    @Autowired
+    private BankingServiceTools bankingServiceTools;
+
+    @Autowired
+    private BankingClient bankingClient;
+
     @PostMapping("/chat")
-    public String chatWithAI(@NotBlank @RequestParam String userInput, @RequestParam String userId) {
+    public String chatWithAI(@NotBlank @RequestParam String userInput) {
 
         var tools = ToolCallbacks.from(
-                new DateTimeTools()
+                new DateTimeTools(),
+                bankingServiceTools
         );
 
         return this.chatClient.prompt()
                 .toolCallbacks(tools)
                 .user(userInput)
-                .advisors( a -> a.param(ChatMemory.CONVERSATION_ID, userId))
+                .advisors( a -> a.param(ChatMemory.CONVERSATION_ID, applicationSecurityContext.getCurrentUser()))
                 .call()
                 .content();
     }
+
+    @GetMapping("/my-accounts")
+    public Object getMyAccounts() {
+        return bankingClient.fetchMyAccounts().block().getBody();
+    }
+    
 
 }
